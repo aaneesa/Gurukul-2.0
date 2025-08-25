5bd6be923255d9bc4e36ce3b410b1a7493cb8b9f
aaneesa <anweshaa002@gmail.com>
2025-08-25 16:07:52 +0530

    routes done

--- app/backend/routes/flashcards.js
+++ app/backend/routes/flashcards.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const Flashcard = require('../models/flashcard');
 const { ClerkExpressRequireAuth } = require('@clerk/clerk-sdk-node');
 const { generateFlashcards, getFlashcardsByChapter } = require('../controllers/apicontroller');
-
-// Get flashcards for a chapter
 router.get('/:chapterId', ClerkExpressRequireAuth(), getFlashcardsByChapter);
-
-// Generate flashcards using AI
 router.post('/generate', ClerkExpressRequireAuth(), generateFlashcards);
 
 module.exports = router;
